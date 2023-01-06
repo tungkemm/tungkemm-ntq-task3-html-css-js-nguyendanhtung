ef70091252776db242371e5b5d05a8698af593df
Nguyen Danh Tung <tung.nguyen14@ntq-solution.com.vn>
2023-01-06 11:41:28 +0700

    complete ex

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { EMPLOYEES } from "./MOCK_DATA.js";
 
 //////// get element
 const $ = document.querySelector.bind(document);
+const contentEl = $(".content");
 const employeeListEl = $(".content-staff");
 const logoPageEl = $(".header-logo");
 const currentPageEl = $(".header-option-page-info span:nth-child(1)");
@@ -14,6 +15,7 @@ const btnSearchEl = $(".header-option-searchicon");
 const sortPageIdEl = $(".header-sort-item-id");
 const sortPageAZEl = $(".header-sort-item1");
 const sortPageZAEl = $(".header-sort-item2");
+const recordSelectEl = $(".header-option-record-list");
 const btnOpenModalAdd = $(".header-option-add");
 const modalAddEl = $(".modal-block");
 const modalFormEl = $(".modal-add-staff");
@@ -25,19 +27,31 @@ const btnAddEl = $(".modal-add-staff-submit button");
 const notiSortEl = $(".sideBar-info-sort");
 const notiSortDetailEl = $(".sideBar-info-sort span");
 
+const contentEmpty = document.createElement("div");
+
 ///////// khai bao phan trang
 const listEmployee = EMPLOYEES;
+let perPage = 40; // so employee trong 1 page
 let currentPage = 1; // page hien tai
-const perPage = 40; // so employee trong 1 page
 let totalPage = Math.ceil(listEmployee.length / perPage); // tong so page
 let perEmployees = listEmployee.slice(
   (currentPage - 1) * perPage,
   (currentPage - 1) * perPage + perPage
-); //  listEmployee trong 1 page
+); //  list employee trong 1 page
 
 ///////// khai bao search
 let inputValueSearch = "";
 let filterListEmployee = [];
+
+///////// arr css btn prev and next when onclick
+const cssBtnWhenAddOrSortOrSearch = [
+  prevPageEl,
+  0.4,
+  "not-allowed",
+  nextPageEl,
+  1,
+  "pointer",
+];
 
 const app = {
   // ham render
@@ -112,6 +126,14 @@ const app = {
     );
   },
 
+  // ham update page khi thay doi record
+  updatePageWhenChangeOptionRecord: function (value, listdata) {
+    perPage = Number(value);
+    currentPage = 1;
+    perEmployees = this.updatePerEmployees(listdata);
+    totalPage = Math.ceil(listdata.length / perPage);
+  },
+
   // ham update currentPage, perEmployees when next
   updatePageWhenNext: function (listdata) {
     currentPage = currentPage + 1;
@@ -131,7 +153,7 @@ const app = {
     totalPage = Math.ceil(listdata.length / perPage);
   },
 
-  // ham loai bo dau tieng viet trong 1 chuoi
+  // ham delete dau tieng viet
   convertName: (name) => {
     name = name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     name = name.replace(/Đ/g, "D");
@@ -140,7 +162,7 @@ const app = {
   },
 
   // ham get input email tu value input name (form add nhan vien)
-  inputEmailWhenAdd: function (input) {
+  getInputEmail: function (input) {
     // input name -> input email
     let inputEmail;
     let inputEmailNew;
@@ -162,31 +184,25 @@ const app = {
 
     // filter ra arr gom nhung email trung voi email input
     const filterCoincidentEmail = listEmployee.filter((employee) => {
-      const isEmail = typeof employee.email === "boolean";
-      const g = !isEmail && employee.email.replace(/[0-9]/g, "");
+      const isCheckTypeEmail = typeof employee.email === "boolean";
+      const g = !isCheckTypeEmail && employee.email.replace(/[0-9]/g, "");
       return g === inputEmailNew;
     });
 
     // arr gom nhung so trong list email trung ten
     const sliceNumberCoincidentEmail = filterCoincidentEmail.map((item) => {
-      const f = typeof item.email === "boolean";
-      if (!f) {
-        return item.email.replace(/[^0-9]/g, "");
+      const isCheckTypeEmail = typeof item.email === "boolean";
+      if (!isCheckTypeEmail) {
+        return Number(item.email.replace(/[^0-9]/g, ""));
       }
     });
 
     // update lai email input
     if (filterCoincidentEmail.length) {
-      let numberEmail;
-      const arrNumberEmail = sliceNumberCoincidentEmail.sort((a, b) =>
-        a.localeCompare(b)
-      );
-      if (arrNumberEmail[arrNumberEmail.length - 1] !== "") {
-        numberEmail = Number(arrNumberEmail[arrNumberEmail.length - 1]) + 1;
-      } else {
-        numberEmail = 1;
-      }
-      inputEmail = `${inputEmail}${numberEmail}@ntq-solution.com.vn`;
+      const arrNumberEmail = sliceNumberCoincidentEmail.sort((a, b) => a - b);
+      inputEmail = `${inputEmail}${
+        arrNumberEmail[arrNumberEmail.length - 1] + 1
+      }@ntq-solution.com.vn`;
     } else {
       inputEmail = inputEmailNew;
     }
@@ -194,8 +210,8 @@ const app = {
     return inputEmail;
   },
 
-  // ham active hide noti sort
-  handleNotiSort: function (element, text) {
+  // ham active and hide noti sort
+  getNotiSort: function (element, text) {
     element.setAttribute("style", "left:0");
     notiSortDetailEl.innerHTML = `${text}`;
 
@@ -204,126 +220,226 @@ const app = {
     }, 3000);
   },
 
-  handleEvents: function () {
-    ////// khi click btn next page
-    nextPageEl.onclick = function () {
-      if (inputValueSearch) {
-        if (currentPage < totalPage) {
-          app.updatePageWhenNext(filterListEmployee);
-          app.render();
-        } else {
-          alert("Khong the next them");
-        }
-      } else {
-        if (currentPage < totalPage) {
-          app.updatePageWhenNext(listEmployee);
-          app.render();
-        } else {
-          alert("Khong the next them");
-        }
-      }
-    };
+  // ham css btn prev or next when not active
+  cssBtnPrevOrNext: function (element, opacity, cursor) {
+    element.setAttribute("style", `opacity: ${opacity}; cursor: ${cursor}`);
+  },
 
-    /////// khi clikc btn prev page
-    prevPageEl.onclick = function () {
-      if (inputValueSearch) {
-        if (currentPage > 1) {
-          app.updatePageWhenPrev(filterListEmployee);
-          app.render();
-        } else {
-          alert("Khong the prev them");
-        }
-      } else {
-        if (currentPage > 1) {
-          app.updatePageWhenPrev(listEmployee);
-          app.render();
-        } else {
-          alert("Khong the prev them");
-        }
-      }
-    };
+  // ham update css btn prev and btn next when add or sort or search
+  updateCssBtnWhenAddOrSortOrSearch: function (
+    elPrev,
+    opacity,
+    notAllowed,
+    elNext,
+    active,
+    pointer
+  ) {
+    this.cssBtnPrevOrNext(elPrev, opacity, notAllowed);
+    this.cssBtnPrevOrNext(elNext, active, pointer);
+    if (currentPage >= totalPage) {
+      this.cssBtnPrevOrNext(elNext, opacity, notAllowed);
+    }
+  },
 
-    /////// khi click btn search
-    btnSearchEl.onclick = function () {
-      inputValueSearch = app
-        .convertName(inputSearchEl.value)
-        .trim()
-        .toLowerCase();
-      if (inputValueSearch) {
-        filterListEmployee = listEmployee.filter((employee) => {
-          return (
-            app
-              .convertName(employee.name)
-              .toLowerCase()
-              .includes(inputValueSearch) ||
-            (employee.email &&
-              employee.email.toLowerCase().includes(inputValueSearch)) ||
-            employee.job.toLowerCase().includes(inputValueSearch)
-          );
+  // ham update css btn prev and btn next when next
+  updateCssBtnPrevWhenNext: function (
+    elPrev,
+    opacity,
+    notAllowed,
+    elNext,
+    active,
+    pointer
+  ) {
+    this.cssBtnPrevOrNext(elPrev, active, pointer);
+    if (currentPage === totalPage) {
+      this.cssBtnPrevOrNext(elNext, opacity, notAllowed);
+    }
+  },
+
+  // ham update css btn next and btn next when prev
+  updateCssBtnNextWhenPrev: function (
+    elPrev,
+    opacity,
+    notAllowed,
+    elNext,
+    active,
+    pointer
+  ) {
+    this.cssBtnPrevOrNext(elNext, active, pointer);
+    if (currentPage === 1) {
+      this.cssBtnPrevOrNext(elPrev, opacity, notAllowed);
+    }
+  },
+
+  // ham handle search nhan vien
+  handleSearchEmployee: function (search) {
+    inputValueSearch = this.convertName(search.value).trim().toLowerCase();
+    if (inputValueSearch) {
+      filterListEmployee = listEmployee.filter((employee) => {
+        return (
+          this.convertName(employee.name)
+            .toLowerCase()
+            .includes(inputValueSearch) ||
+          (employee.email &&
+            employee.email.toLowerCase().includes(inputValueSearch)) ||
+          employee.job.toLowerCase().includes(inputValueSearch)
+        );
+      });
+      if (filterListEmployee.length === 0) {
+        contentEmpty.innerHTML = `
+          <div class="content-notfound">
+            <div class="sketch">
+              <div class="bee-sketch red"></div>
+              <div class="bee-sketch blue"></div>
+            </div>
+            <h1 class="content-notfound-title">404
+            <small class="content-notfound-noti">No Search Results Found</small></h1>
+          </div>
+        `;
+        contentEl.appendChild(contentEmpty);
+      } else {
+        contentEmpty.remove();
+      }
+      this.updatePageWhenSearchOrSortOrAdd(filterListEmployee);
+      this.render();
+      this.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+    } else {
+      alert("Chua nhap gi trong input search");
+    }
+  },
+
+  // ham handle add nhan vien
+  handleAddEmployee: function (name, position) {
+    const inputNameAdd = name.value.trim();
+    const selectPositionAdd = position.value.trim();
+    if (inputNameAdd && selectPositionAdd) {
+      if (inputNameAdd.replace(/[^0-9]/g, "").length === 0) {
+        // select id nhan vien cuoi cung
+        const arrId = [];
+        listEmployee.forEach((employee) => arrId.push(employee.id));
+        const id = arrId.sort()[arrId.sort().length - 1];
+
+        // input email
+        const inputEmail = this.getInputEmail(inputNameAdd);
+
+        // add nhan vien
+        listEmployee.unshift({
+          id: id + 1,
+          name: inputNameAdd,
+          email: inputEmail,
+          job: selectPositionAdd,
         });
-        if (filterListEmployee.length === 0) {
-          alert("Khong tim thay thong tin nhan vien nao !!!");
-        } else {
-          app.updatePageWhenSearchOrSortOrAdd(filterListEmployee);
-          app.render();
-        }
+
+        inputNameAddEl.value = "";
+        selectPositionAddEl.value = "Team Leader";
+        inputEmailAddEl.value = "";
+        inputValueSearch = "";
+        inputSearchEl.value = "";
+        modalAddEl.classList.remove("active");
+        modalAddEl.classList.add("hide");
+
+        this.updatePageWhenSearchOrSortOrAdd(listEmployee);
+        this.render();
+        this.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+        contentEmpty.remove();
       } else {
-        alert("Chua nhap gi trong input search");
+        alert("Ten nhan vien khong duoc nhap so");
+      }
+    } else {
+      alert("Chua nhap day du thong tin");
+    }
+  },
+
+  /////////////// xu ly su kien
+  start: function () {
+    ///// khi load trang
+    window.onload = function () {
+      app.render();
+      app.getNotiSort(notiSortEl, "Original Data");
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+    };
+
+    ///// khi change select record
+    recordSelectEl.onchange = function (e) {
+      const newPerPage = e.target.value.trim();
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      app.updatePageWhenChangeOptionRecord(newPerPage, _listEmployee);
+      app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+    };
+
+    ///// khi click btn next page
+    nextPageEl.onclick = function () {
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      if (currentPage < totalPage) {
+        app.updatePageWhenNext(_listEmployee);
+        app.render();
+        app.updateCssBtnPrevWhenNext(...cssBtnWhenAddOrSortOrSearch);
       }
     };
 
-    ////// khi click btn delete text search
+    ////// khi click btn prev page
+    prevPageEl.onclick = function () {
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      if (currentPage > 1) {
+        app.updatePageWhenPrev(_listEmployee);
+        app.render();
+        app.updateCssBtnNextWhenPrev(...cssBtnWhenAddOrSortOrSearch);
+      }
+    };
+
+    ////// khi click btn search
+    btnSearchEl.onclick = function () {
+      app.handleSearchEmployee(inputSearchEl);
+    };
+
+    ///// khi click btn delete text search
     btnDeleteSearchEl.onclick = function () {
       inputValueSearch = "";
       inputSearchEl.value = "";
       app.updatePageWhenSearchOrSortOrAdd(listEmployee);
       app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+      contentEmpty.remove();
     };
 
-    ////// khi click vao logo NTQ
+    ///// khi click logo NTQ
     logoPageEl.onclick = function () {
       inputValueSearch = "";
       app.updatePageWhenSearchOrSortOrAdd(listEmployee);
       app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
+      contentEmpty.remove();
     };
 
-    /////// khi click sort A-Z
+    ////// khi click sort A-Z
     sortPageAZEl.onclick = function () {
-      if (inputValueSearch) {
-        app.sortListDataAZ(filterListEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(filterListEmployee);
-      } else {
-        app.sortListDataAZ(listEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(listEmployee);
-      }
-      app.handleNotiSort(notiSortEl, "A-Z");
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      app.sortListDataAZ(_listEmployee);
+      app.updatePageWhenSearchOrSortOrAdd(_listEmployee);
+      app.getNotiSort(notiSortEl, "A-Z");
       app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
     };
 
-    /////// khi click sort Z-A
+    ////// khi click sort Z-A
     sortPageZAEl.onclick = function () {
-      if (inputValueSearch) {
-        app.sortListDataZA(filterListEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(filterListEmployee);
-      } else {
-        app.sortListDataZA(listEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(listEmployee);
-      }
-      app.handleNotiSort(notiSortEl, "Z-A");
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      app.sortListDataZA(_listEmployee);
+      app.updatePageWhenSearchOrSortOrAdd(_listEmployee);
+      app.getNotiSort(notiSortEl, "Z-A");
       app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
     };
 
-    /////// khi click sort Id
+    ////// khi click sort Id
     sortPageIdEl.onclick = function () {
-      if (inputValueSearch) {
-        app.sortListDataId(filterListEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(filterListEmployee);
-      } else {
-        app.sortListDataId(listEmployee);
-        app.updatePageWhenSearchOrSortOrAdd(listEmployee);
-      }
-      app.handleNotiSort(notiSortEl, "Id");
+      let _listEmployee = inputValueSearch ? filterListEmployee : listEmployee;
+      app.sortListDataId(_listEmployee);
+      app.updatePageWhenSearchOrSortOrAdd(_listEmployee);
+      app.getNotiSort(notiSortEl, "Id");
       app.render();
+      app.updateCssBtnWhenAddOrSortOrSearch(...cssBtnWhenAddOrSortOrSearch);
     };
 
     /////// khi active modal add
@@ -332,85 +448,37 @@ const app = {
       modalAddEl.classList.add("active");
     };
 
-    ///// khi hide modal add bang icon
+    ////// khi hide modal add bang icon
     closeModalAddEl.onclick = function (e) {
       e.stopImmediatePropagation();
       modalAddEl.classList.remove("active");
       modalAddEl.classList.add("hide");
     };
 
-    ///// hide modal add khi click vao background
+    ///// hide form add khi click vao background modal add
     modalAddEl.onclick = function () {
       modalAddEl.classList.remove("active");
       modalAddEl.classList.add("hide");
     };
 
-    ///// ngan noi bot khi click vao background modal
+    ///// ngan noi bot khi click vao form add nhan vien
     modalFormEl.onclick = (e) => {
       e.stopPropagation();
     };
 
-    ///// khi blur input name (form add nhan vien)
-    inputNameAddEl.onblur = function () {
+    ///// khi change input name (form add nhan vien)
+    inputNameAddEl.onkeyup = function () {
       const inputNameAdd = inputNameAddEl.value.trim();
       if (inputNameAdd) {
-        const inputEmail = app.inputEmailWhenAdd(inputNameAdd);
+        const inputEmail = app.getInputEmail(inputNameAdd);
         inputEmailAddEl.value = inputEmail;
       }
     };
 
     ///// khi click btn add nhan vien
     btnAddEl.onclick = function () {
-      const inputNameAdd = inputNameAddEl.value.trim();
-      const selectPositionAdd = selectPositionAddEl.value.trim();
-      if (inputNameAdd && selectPositionAdd) {
-        if ((inputNameAdd.replace(/[^0-9]/g, "").length === 0)) {
-          // select id nhan vien cuoi cung
-          const arrId = [];
-          listEmployee.forEach((employee) => arrId.push(employee.id));
-          const id = arrId.sort()[arrId.sort().length - 1];
-
-          // input email
-          const inputEmail = app.inputEmailWhenAdd(inputNameAdd);
-
-          // add nhan vien
-          listEmployee.unshift({
-            id: id + 1,
-            name: inputNameAdd,
-            email: inputEmail,
-            job: selectPositionAdd,
-          });
-
-          inputNameAddEl.value = "";
-          selectPositionAddEl.value = "Team Leader";
-          inputEmailAddEl.value = "";
-          inputValueSearch = "";
-          inputSearchEl.value = "";
-          modalAddEl.classList.remove("active");
-          modalAddEl.classList.add("hide");
-
-          app.updatePageWhenSearchOrSortOrAdd(listEmployee);
-          app.render();
-        } else {
-          alert("Ten nhan vien khong duoc nhap so");
-        }
-      } else {
-        alert("Chua nhap day du thong tin");
-      }
+      app.handleAddEmployee(inputNameAddEl, selectPositionAddEl);
     };
-
-    ///// khi load trang
-    window.onload = function () {
-      app.handleNotiSort(notiSortEl, "Original Data");
-    };
-  },
-
-  start: function () {
-    // Xu ly su kien
-    this.handleEvents();
-
-    // Render employee list
-    this.render();
   },
 };
 
